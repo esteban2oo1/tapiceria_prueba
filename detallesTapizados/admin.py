8c3313476f4ec18128ced61f176e0e1806d79a58
mclguerrero <maycolguerrero2021@itp.edu.co>
2023-12-04 21:38:53 -0500

    Se actualizo el modelo DetallesTapizados

--- detallesTapizados/admin.py
+++ detallesTapizados/admin.py
@@ -4,5 +4,5 @@ from detallesTapizados.models import DetallesTapizados
 @admin.register(DetallesTapizados)
 
 class DetallesTapizadosAdmin(admin.ModelAdmin):
-    list_display=['id','tipoTela','color','producto','tipoEspumilla','cantidad_telasColores','cantidad_tipoMaterial']
+    list_display=['id','tipoTela','color','producto','tipoEspumilla','cantidad_telaColor','cantidad_tipoMaterial', 'cantidad_tipoEspumilla' ]
     
