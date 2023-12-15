65cefca27e5b3876661648bdf5c385344b0bcd4b
esteban2oo1 <jossajhon123454@gmail.com>
2023-12-14 23:08:07 -0500

    usuarios admin

--- users/models.py
+++ users/models.py
@@ -2,11 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, Group
 
 class User(AbstractUser):
-    email=models.EmailField(unique=True)    
-    USERNAME_FIELD="email"
-    REQUIRED_FIELDS=[]
-    
-    groups = models.ManyToManyField(Group, related_name='users', blank=True)
+    pass
 """
 
 #
