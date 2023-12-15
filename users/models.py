557d2ddae6501e34fcb6ba39ac2b1a0a4a6d3d4d
esteban2oo1 <jossajhon123454@gmail.com>
2023-12-15 13:26:46 -0500

    usuarios admin

--- users/models.py
+++ users/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, Group
 
 class User(AbstractUser):
-    pass
+    email=models.EmailField(unique=True)    
+    USERNAME_FIELD="email"
+    REQUIRED_FIELDS=[]
 """
 
 #
