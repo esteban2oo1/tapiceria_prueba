ee4e36cf9564b4eba3acd95bc89b22b16ee0d4fb
esteban2oo1 <jossajhon123454@gmail.com>
2023-12-15 18:27:18 -0500

    restf

--- tapiceria/settings.py
+++ tapiceria/settings.py
@@ -44,11 +44,6 @@ INSTALLED_APPS = [
     'creditos',
     'abonos',
 ]
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
-}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
